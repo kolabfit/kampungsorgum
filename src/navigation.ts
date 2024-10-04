0507b4184fdbb0fb58e16c951afd6752725ce04a
Daud Nugraha <daudelangnugraha@gmail.com>
2024-10-05 00:36:48 +0700

    beranda, profil, edutainment, kontak, navigasi done

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,31 @@
 import { getPermalink } from './utils/permalinks';
 
+import { contentfulClient } from '~/lib/contentful';
+import type { EntryFieldTypes } from 'contentful';
+
+interface KontakType {
+  contentTypeId: string;
+  fields: {
+    logo: EntryFieldTypes.Text;
+    usernameKontak: EntryFieldTypes.Text;
+    url: EntryFieldTypes.Text;
+  };
+}
+
+// Fetch the entry and include linked assets
+const entries = await contentfulClient.getEntries<KontakType>({
+  content_type: 'kontakListType',
+});
+
+const items = entries.items.map((item) => {
+  return {
+    ariaLabel: item.fields.usernameKontak,
+    icon: item.fields.logo,
+    href: item.fields.url,
+    target: '_blank',
+  };
+});
+
 export const headerData = {
   links: [
     {
@@ -106,24 +132,8 @@ export const footerData = {
       ],
     },
   ],
-  secondaryLinks: [
-    // { text: 'Terms', href: getPermalink('/terms') },
-    // { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
   socialLinks: [
-    // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    {
-      ariaLabel: 'Instagram',
-      icon: 'tabler:brand-instagram',
-      href: 'https://www.instagram.com/kampungsorgum.bojongmanggu/',
-      target: '_blank',
-    },
-    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    ...items,
   ],
-  // footNote: `
-  //   <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-  //   Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  // `,
+  secondaryLinks: [],
 };
